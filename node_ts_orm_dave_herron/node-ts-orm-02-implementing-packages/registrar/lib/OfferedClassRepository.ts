055ecba3f5fe67526a16be4a74892728f1f07f6f
kkesseler <kkesseler@ifeelsmart.com>
2024-09-24 23:54:02 +0530

    nrdp-02-node-ts-orm-dh - project1-registrar-app - CRUD ops and setting entity for testing continued.

--- node_ts_orm_dave_herron/node-ts-orm-02-implementing-packages/registrar/lib/OfferedClassRepository.ts
+++ node_ts_orm_dave_herron/node-ts-orm-02-implementing-packages/registrar/lib/OfferedClassRepository.ts
@@ -1,6 +1,49 @@
 import { EntityRepository, Repository } from "typeorm";
+import * as util from "util"
+import * as yaml from "js-yaml"
+import { promises as fs } from "fs"
+
 import { OfferedClass } from "./entities/OfferedClasses";
 
+class OfferedClassYAML {
+  classes: Array<OfferedClass>
+}
+
 @EntityRepository(OfferedClass)
-export class OfferedClassRepository extends Repository<OfferedClass> {}
+export class OfferedClassRepository extends Repository<OfferedClass> {
+  async all_classes(): Promise<OfferedClass []> {
+    let classes = await this.find({
+      relations: ["students"]
+    })
+
+    return classes
+  }
+  async update_classes(classFN: string) {
+    const yaml_text = await fs.readFile(classFN, "utf8")
+    const offered = (yaml.load(yaml_text, {
+      filename: classFN
+    }) as OfferedClassYAML)
+
+    if (typeof offered !== "object" || !Array.isArray(offered.classes)) {
+      throw new Error(`update_classes read incorrect data file from ${classFN}`)
+    }
+
+    let all = await this.all_classes()
+
+    for (let cls of all) {
+      let still_offered = false
+
+      for (let ofrd of offered.classes) {
+        if (ofrd.code === cls.code) {
+          still_offered = true
+          break
+        }
+      }
+
+      if (!still_offered) {
+        this.delete_offered_class(cls.code)
+      }
+    }
+  }
+}
 
